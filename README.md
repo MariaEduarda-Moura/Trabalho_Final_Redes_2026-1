154fba93e8216c5b6d8bd0bc863ed63c26960e69
sgmaykon <ultra@Nokyam.(sgmaykon@estudante.ufscar.br)>
2026-06-14 23:27:31 -0300

    feat: ARP table and routing

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Trabalho_Final_Redes_2026-1
+
+
+Para testar as tabelas de roteamento, faça 'gcc table.c table_structs.c'
+`./a.out`
+
+Falta integrar as rotas com os pacotes...
---
+++ table_structs.h
@@ -0,0 +1,60 @@
+#ifndef DIJKSTRA_H
+#define DIJKSTRA_H
+
+#include <limits.h>
+
+// Tamanho do endereço n+1 para facilitar manipulação de strings
+#define END_SIZE 17 
+
+typedef struct Router Router;
+typedef struct Net_link Net_link;
+
+typedef struct{
+  Router *neighbor;
+} phy_link;
+
+struct Net_link{
+    int cost;
+    Router *a;
+    Router *b;
+};
+
+typedef struct{
+  char rede_dest[END_SIZE];
+  char mask[END_SIZE];
+  char next_hop[END_SIZE];
+  int cost;
+} route_table_entry;
+
+struct Router{
+  char endereco [END_SIZE];
+  char* nome;
+  char interface [6];
+  int rt_num;
+  int link_num; 
+  int rt_max;
+  int link_max;
+  route_table_entry * rt; 
+  phy_link* p_link; 
+};
+
+typedef struct Network{
+    Router **routers;
+    int router_count;
+    int router_capacity;
+    Net_link **links;
+    int link_count;
+    int link_capacity;
+} Network;
+
+typedef struct{
+    int *dist;
+    int *prev;
+    int count;
+} SPF_Result;
+
+SPF_Result* dijkstra(Network *net, Router *source);
+
+void free_spf_result(SPF_Result *result);
+
+#endif
