90dce120c399fad0910fe6c9564066c13b3814d1
sgmaykon <ultra@Nokyam.(sgmaykon@estudante.ufscar.br)>
2026-06-22 23:13:24 -0300

    feat: makefile main and gitignore

---
+++ pacotes.h
@@ -0,0 +1,21 @@
+#include <stdint.h>
+
+typedef struct {
+    char ip_origem[16];  // Guarda o IP de quem envia. Tem tamanho 16 para caber o formato "XXX.XXX.XXX.XXX" + o fim do texto (\0)
+    char ip_destino[16]; // Guarda o IP de quem vai receber.
+    int ttl;             // Time to Live (Tempo de Vida) - número inteiro que diminui a cada salto
+    char dados[100];     // O conteúdo/mensagem que o pacote carrega (limite de 100 caracteres)
+} Pacote;
+
+typedef struct {
+    char rede_dest[16];
+    char mask[16];
+    char next_hop[16];
+} RotaTeste;
+
+Pacote criar_pacote(char *origem, char *destino, int ttl, char *conteudo);
+void imprimir_pacote(Pacote p);
+uint32_t ip_para_int(const char *ip_str);
+int contar_prefixo(const char *mask_str);
+void encaminhar_pacote(Pacote *p);
+int pacote_menu();
